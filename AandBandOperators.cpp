007c3299a0c959b1efa4e59167641f26f997c5c9
IsratIJK <iptu.jahan.amin@gmail.com>
2021-03-12 12:26:27 +0600

    A and B operator

---
+++ AandBandOperators.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main() {
+  long long t,a,b,r,i;
+  char c;
+   cin>>t;
+   for(i=1;i<=t;i++){
+       cin>>a>>b>>c;
+
+       
+       if(c=='+'){
+           r=a+b;
+           
+       }
+       else if(c=='-'){
+           r=a-b;
+       }
+        else if(c=='%'){
+           r=a%b;
+       }
+        else if(c=='/'){
+           r=a/b;
+       }
+        else if(c=='*'){
+           r=a*b;
+       }
+        else if(c=='-'){
+           r=a-b;
+       }
+       cout<<"Case"<<" "<<i<<":"<<" "<<r<<endl;
+      
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
